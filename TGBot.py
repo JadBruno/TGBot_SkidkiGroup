e99b0cd8cded2323ba083491f72663080745c03a
Александр Санников <al.nic.sannikov@yandex.ru>
2022-10-27 22:26:00 +0300

    Добавлена обработка кнопок Каталог и Как получить скидки

--- TGBot.py
+++ TGBot.py
@@ -1,3 +1,4 @@
+from cgitb import text
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
@@ -8,37 +9,49 @@ import pymysql.cursors
 bot = Bot(token=config.bot_token.get_secret_value())
 dp = Dispatcher(bot)
 
-db_host = '23.105.226.124'
-db_port = 3306
-db_user = 'tg_bot_test_user'
-db_password = 'bJ3mI2zA2b'
-db_name = 'tg_bot_db'
-db_charset = 'utf8'
-connection = pymysql.connect(host=db_host, port=db_port, user=db_user, password=db_password, db=db_name,
-                                 charset=db_charset, cursorclass=pymysql.cursors.DictCursor)
+#Подключение к базе данных MySQL
+def connect_bd():
+    db_host = '23.105.226.124'
+    db_port = 3306
+    db_user = 'tg_bot_test_user'
+    db_password = 'bJ3mI2zA2b'
+    db_name = 'tg_bot_db'
+    db_charset = 'utf8'
+    connection = pymysql.connect(host=db_host, port=db_port, user=db_user, password=db_password, db=db_name,
+                                    charset=db_charset, cursorclass=pymysql.cursors.DictCursor)
+    return connection
 
-with connection.cursor() as cursor:
-        sql_records = "SELECT `wb_category_name` FROM `tg_wb_categories`"
-        cursor.execute(sql_records)
-        result = cursor.fetchall()
-        
+#Скачивание каталога из БД
+def gain_categories():
+    connection = connect_bd()
+    with connection.cursor() as cursor:
+            sql_records = "SELECT `wb_category_name` FROM `tg_wb_categories`"
+            cursor.execute(sql_records)
+            return cursor.fetchall()
+    
+#Обработка команды /start
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-   kb = [[
-           types.KeyboardButton(text="Каталог"),
-           types.KeyboardButton(text="❓ Как получить скидку")
+    kb = [[
+           types.KeyboardButton(text="📔 Каталог"),
+           types.KeyboardButton(text="❓ Как получить скидку"),
         ],]
-   keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True, one_time_keyboard=True)
-   await message.answer('Добрый день!\n В этом канале мы расскажем Вам как приобрести наши товар на Wildberries со скидкой до 100%\n Посмотрите раздел "Как получить скидку?" или сразу переходите в "Каталог" к выбору товаров.', reply_markup=keyboard)        
-
-@dp.message_handler(commands=['Каталог'])
-async def send_welcome(message: types.Message):
-   for el in result:
-      await message.answer(el["wb_category_name"])
-
-@dp.message_handler() #Создаём новое событие, которое запускается в ответ на любой текст, введённый пользователем.
-async def echo(message: types.Message): #Создаём функцию с простой задачей — отправить обратно тот же текст, что ввёл пользователь.
-    await message.answer(message.text)
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+    await message.answer('Добрый день!\nВ этом канале мы расскажем Вам как приобрести наши товар на Wildberries со скидкой до 100%\nПосмотрите раздел "Как получить скидку?" или сразу переходите в "Каталог" к выбору товаров.', reply_markup=keyboard)        
+    
+#Обработка любого сообщения пользователя
+@dp.message_handler()
+async def reply(message: types.Message):
+    global connection
+    #Обработка нажатия кнопки Каталог
+    if message.text == "📔 Каталог":
+        catalog_categories = gain_categories()
+        for el in catalog_categories:
+            await message.answer(el["wb_category_name"])   
+    
+    #Обработка нажатия кнопки Как получить скидку
+    if message.text == "❓ Как получить скидку":
+        await message.answer("Выберите товар и нажмите на кнопку под ним, кэшбэк зафиксируется в нашей системе\nПерейдите по ссылке на Wildberies и купите его")  
 
 if __name__ == '__main__':
-   executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True)        
