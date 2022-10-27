700e05e4c9bb0dc5ce0399dcafaf8f2d423544cc
Александр Санников <al.nic.sannikov@yandex.ru>
2022-10-27 21:05:27 +0300

    добавлены кнопки

--- TGBot.py
+++ TGBot.py
@@ -1,4 +1,6 @@
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import pymysql
 import pymysql.cursors
 
@@ -17,27 +19,27 @@ connection = pymysql.connect(host=db_host, port=db_port, user=db_user, password=
                                  charset=db_charset, cursorclass=pymysql.cursors.DictCursor)
 
 with connection.cursor() as cursor:
-        sql_records = "SELECT `product_category_name` FROM `product_category`"
+        sql_records = "SELECT `wb_category_name` FROM `tg_wb_categories`"
         cursor.execute(sql_records)
         result = cursor.fetchall()
         
-# from aiogram import types
-@dp.message_handler(commands="start")
-async def cmd_start(message: types.Message):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    buttons = ["С пюрешкой", "Без пюрешки"]
-    keyboard.add(*buttons)
-    await message.answer("Как подавать котлеты?", reply_markup=keyboard)
-        
-@dp.message_handler(commands=['catalog'])
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+   kb = [[
+           types.KeyboardButton(text="📔 Каталог"),
+           types.KeyboardButton(text="❓ Как получить скидку")
+        ],]
+   keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+   await message.reply('Добрый день!\n В этом канале мы расскажем Вам как приобрести наши товар на Wildberries со скидкой до 100%\n Посмотрите раздел "Как получить скидку?" или сразу переходите в "Каталог" к выбору товаров.', reply_markup=keyboard)        
+
+@dp.message_handler(commands=['Каталог'])
 async def send_welcome(message: types.Message):
    for el in result:
       await message.answer(el["product_category_name"])
 
-# @dp.message_handler() #Создаём новое событие, которое запускается в ответ на любой текст, введённый пользователем.
-# async def echo(message: types.Message): #Создаём функцию с простой задачей — отправить обратно тот же текст, что ввёл пользователь.
-#     await message.answer(message.text)
-   
+@dp.message_handler() #Создаём новое событие, которое запускается в ответ на любой текст, введённый пользователем.
+async def echo(message: types.Message): #Создаём функцию с простой задачей — отправить обратно тот же текст, что ввёл пользователь.
+    await message.answer(message.text)
 
 if __name__ == '__main__':
    executor.start_polling(dp, skip_updates=True)
